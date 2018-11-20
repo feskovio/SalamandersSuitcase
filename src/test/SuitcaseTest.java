65b03cb758e71354fc58123ca45d5ef6c1c20d8a
feskovalex@gmail.com <GithubME197588>
2018-11-20 17:32:59 -0500

    added dummy test

---
+++ src/test/SuitcaseTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SuitcaseTest {
+
+    @Test
+    public void testToggleDayTime() throws Exception {
+
+    }
+
+    @Test
+    public void testAddResident() throws Exception {
+
+    }
+
+    @Test
+    public void testSpeak() throws Exception {
+
+    }
+
+    @Test
+    public void testCallByName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTotalFoodWeight() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAvgWeight() throws Exception {
+
+    }
+
+    @Test
+    public void testSelfEnter() throws Exception {
+
+    }
+}
